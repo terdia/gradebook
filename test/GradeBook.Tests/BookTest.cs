6e9d5305be2ee66f3839722f8c196403bf49883e
terdia <terrymarcy2000@yahoo.com>
2020-09-18 06:33:41 +0200

    add more test cases for types

--- test/GradeBook.Tests/BookTest.cs
+++ test/GradeBook.Tests/BookTest.cs
@@ -6,7 +6,7 @@ namespace GradeBook.Tests
     public class BookTest
     {
         [Fact]
-        public void BookCalculateAverageGrade()
+        public void BookCalculatesAnAverageGrade()
         {
             // arrange
             var book = new Book("");
--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,17 +5,130 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+
+        //value type test
         [Fact]
-        public void Test1()
+        public void StringsBehaveLikeValueTypes()
         {
-        
-            var book1 = new GetBook("book1");
-            var book2 = new GetBook("book2");
-            
+            string x = "Scott";
+            var upper = MakeUppercase(x);
+
+            Assert.Equal("Scott", x);
+            Assert.Equal("SCOTT", upper);
+        }
+
+        private string MakeUppercase(string x)
+        {
+            return x.ToUpper();
+        }
+
+        [Fact]
+        public void ValueTypeIsAlwaysPassByValue()
+        {
+            var x = GetInt();
+
+            Assert.Equal(3, x);
+
+            SetInt(ref x);
+            Assert.Equal(10, x);
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        private void SetInt(ref int x)
+        {
+            x = 10;
+        }
+
+
+        //reference type tests
+        [Fact]
+        public void CSharpCanPassByReference()
+        {
+
+            var book = GetBook("book");
+            GetSetNameByRef(ref book, "New name");
+
+            Assert.Equal("New name", book.Name);
+
+        }
+
+
+        private void GetSetNameByRef(ref Book book, string newName)
+        {
+            book = new Book(newName);
+            book.Name = newName;
+        }
+
+
+        [Fact]
+        public void CSharpIsPassByValueByDefault()
+        {
+
+            var book = GetBook("book");
+            GetSetName(book, "New name");
+
+            Assert.NotEqual("New name", book.Name);
+
+        }
+
+        private void GetSetName(Book book, string newName)
+        {
+            book = new Book(newName); // points to a different reference in memory
+            book.Name = newName;
+        }
+
+
+        [Fact]
+        public void CanChangeNameFromReference()
+        {
+
+            var book = GetBook("book");
+            SetName(book, "New name"); //pass by value.
+
+            Assert.Equal("New name", book.Name);
+
+        }
+
+        private void SetName(Book book, string newName)
+        {
+            book.Name = newName;
+        }
+
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+
+            var book1 = GetBook("book1");
+            var book2 = GetBook("book2");
+
+            Assert.Equal("book1", book1.Name);
+            Assert.Equal("book2", book2.Name);
+            Assert.NotSame(book1, book2);
+
+        }
+
+
+        [Fact]
+        public void TwoVariabesCanRefSameObjects()
+        {
+
+            var book1 = GetBook("book1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+
+        }
+
+        private Book GetBook(string name)
+        {
+            return new Book(name);
         }
     }
 
-    private Book GetBook(string name) {
-        return new Book(name);
-    }
 }
